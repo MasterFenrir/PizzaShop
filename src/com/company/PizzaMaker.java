7e2ac4c1e9505176b0afef071e08988dad5cd4dc
Wessel Becker <wesselbecker92@gmail.com>
2015-09-21 17:35:14 +0200

    added a test to main

--- src/com/company/PizzaMaker.java
+++ src/com/company/PizzaMaker.java
@@ -4,6 +4,10 @@ public class PizzaMaker {
 
     public static void main(String[] args) {
         Pizza basicPizza = new TomatoSauce(new Mozzarella(new PlainPizza()));
+        basicPizza.setSize("S");
+        System.out.println("Ingredients: " + basicPizza.getDescription());
+        System.out.println("Price: " + basicPizza.getCost());
+        basicPizza.setSize("L");
         System.out.println("Ingredients: " + basicPizza.getDescription());
         System.out.println("Price: " + basicPizza.getCost());
     }
